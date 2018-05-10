5acd8cb6b2aca535b7eb810b6768e051c6c90404
Jesse Walsh <jrwalsh@iastate.edu>
2018-05-10 17:18:59 -0500

    Created a functions file

---
+++ data-raw/R/functions.R
@@ -0,0 +1,9 @@
+####################################################################################################
+## Project:         MaizeOmics
+## Script purpose:  Create functions to be sourced and used for this project
+##
+## Input:
+## Output:
+## Date: 2018-05-10
+## Author: Jesse R. Walsh
+####################################################################################################
